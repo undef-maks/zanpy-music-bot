860e7e4d24571a100180e1f58d480a376ad7e907
undef-maks <maksym.sliuzar@gmail.com>
2026-02-13 05:49:28 +0200

    Delete src/test-adapter.ts

--- src/test-adapter.ts
+++
@@ -1,8 +0,0 @@
-import { parseResourceUrl } from "./core/url-parser";
-import { SoundcloudAdapter } from "adapters/sc.adapter";
-
-
-const sc = new SoundcloudAdapter();
-const parsed = parseResourceUrl("https://soundcloud.com/leonardo-patron-chavarria/sets/hyperbaiter");
-
-console.log(parsed);
